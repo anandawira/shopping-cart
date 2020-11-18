9f6b82cd05c48c7835bf0a40e92e5b975b262166
Ananda Wiradharma <anandawira754@gmail.com>
2020-11-18 21:00:56 +0700

    change icon to import from node modules

--- src/component/Detail.jsx
+++ src/component/Detail.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import '../style/detail.css';
 import items from '../items';
-import cartIcon from '../assets/shopping_cart-white-18dp.svg'
+import { FaShoppingCart } from 'react-icons/fa';
 
 export default function Detail({ match }) {
   const key = parseInt(match.params.id);
   const item = items.get(key);
   return (
     <div id="detail">
-      <img id='prodImg' src={item.image} alt="product" />
+      <img id="prodImg" src={item.image} alt="product" />
       <div id="info">
         <h1>{item.name}</h1>
         <p>${item.price}.00</p>
         <div id="addBtn">
-          <img src={cartIcon} alt="add"/>
+          <FaShoppingCart id="cartIcon" />
           <p>ADD TO CART</p>
         </div>
       </div>
