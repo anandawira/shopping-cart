49be4ef89fd54d0f6e4260a9e37ebfb21c0a6f30
Ananda Wiradharma <anandawira754@gmail.com>
2020-11-18 23:48:08 +0700

    add dim effect on cart when new item added, change header text color to #eaeaea

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style/header.scss';
 import { Link } from 'react-router-dom';
 import { FaShoppingCart } from 'react-icons/fa';
 
 export default function Header(props) {
+  const [isDim, setIsDim] = useState(false);
+
+  useEffect(() => {
+    if (props.itemCount == '0') {
+      return;
+    }
+    console.log('hook called');
+    setIsDim(true);
+    setTimeout(() => {
+      setIsDim(false);
+    }, 200);
+  }, [props.itemCount]);
+
   return (
     <header>
       <h1>NSFW SHOP</h1>
@@ -17,7 +30,7 @@ export default function Header(props) {
           </li>
         </ul>
       </nav>
-      <div id="cart">
+      <div id="cart" className={isDim ? 'dim' : ''}>
         <FaShoppingCart id="cartIcon" />
         <p id="cartCount">{props.itemCount}</p>
       </div>
